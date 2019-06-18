624b7233bcd10aa546d07a4296d759b0a40e6bd5
Sam Curren <telegramsam@gmail.com>
2019-06-18 06:12:27 -0600

    fill out tests
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

--- indypython/wallet.py
+++ indypython/wallet.py
@@ -6,31 +6,36 @@ import json
 async def test_method(arg1, arg2):
     return None
 
+
 async def create_wallet(config: Dict,
                         credentials: Dict) -> None:
     return await wallet.create_wallet(json.dumps(config), json.dumps(credentials))
 
-async def open_wallet(config: str,
-                      credentials: str) -> int:
-    pass
+
+async def open_wallet(config: Dict,
+                      credentials: Dict) -> int:
+    return await wallet.open_wallet(json.dumps(config), json.dumps(credentials))
+
 
 async def close_wallet(handle: int) -> None:
-    pass
+    return await wallet.close_wallet(handle)
 
-async def delete_wallet(config: str,
-                        credentials: str) -> None:
-    pass
+
+async def delete_wallet(config: Dict,
+                        credentials: Dict) -> None:
+    return await wallet.delete_wallet(json.dumps(config), json.dumps(credentials))
+
 
 async def export_wallet(handle: int,
-                        export_config_json: str) -> None:
-    pass
+                        export_config: Dict) -> None:
+    return await wallet.export_wallet(handle, json.dumps(export_config))
 
 
-async def import_wallet(config: str,
-                        credentials: str,
-                        import_config_json: str) -> None:
-    pass
+async def import_wallet(config: Dict,
+                        credentials: Dict,
+                        import_config: Dict) -> None:
+    return await wallet.import_wallet(json.dumps(config), json.dumps(credentials), json.dumps(import_config))
 
-async def generate_wallet_key(config: Optional[str]) -> str:
-    pass
 
+async def generate_wallet_key(config: Optional[Dict]) -> Dict:
+    return json.loads(await wallet.generate_wallet_key(json.dumps(config)))
